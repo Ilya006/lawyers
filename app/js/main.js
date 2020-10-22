0ae27bba8b989467a2fce88d3954f32a44897424
ваше имя <Senognoevilya@gmail.com>
2020-10-22 20:56:03 +0500

    add section hero

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
   let menuBurger = document.querySelector('.menu__burger');
   let headerNav = document.querySelector('.header__navigation');
 
-  // добавляем класс action при нажатии на 
+  // добавляем класс action при нажатии на burger
   menuBurger.onclick = function() {
     headerNav.classList.toggle('active');
     menuBurger.classList.toggle('active');
   };
+
 
 }); //end 
