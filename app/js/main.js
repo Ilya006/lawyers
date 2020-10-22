f8dbe1568aa79b28d69e3e73386b0a6ad9d3cd37
ваше имя <Senognoevilya@gmail.com>
2020-10-22 19:26:53 +0500

    add header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  let menuBurger = document.querySelector('.menu__burger');
+  let headerNav = document.querySelector('.header__navigation');
 
+  // добавляем класс action при нажатии на 
+  menuBurger.onclick = function() {
+    headerNav.classList.toggle('active');
+    menuBurger.classList.toggle('active');
+  };
 
 }); //end 
